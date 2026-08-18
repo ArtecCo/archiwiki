941ec389650070c1fb28b06db7ff5d07ae08a653
Artec.Co <artec.co.online@gmail.com>
2026-08-18 11:21:27 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import InviteAdmin from "./components/InviteAdmin.jsx";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext.jsx";
 
+const isInviteAdmin =
+  window.location.pathname === "/invite-admin";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    {isInviteAdmin ? (
+      <InviteAdmin />
+    ) : (
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    )}
   </React.StrictMode>
 );
