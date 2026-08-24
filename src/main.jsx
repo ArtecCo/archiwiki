60ff4fb1b64cee107c51876a1e4b280d702b7440
Artec.Co <artec.co.online@gmail.com>
2026-08-24 11:41:41 +0530

    Enable same-note header anchors

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "./sameNoteAnchors.js";
 import { AuthProvider } from "./context/AuthContext.jsx";
 
 const root = ReactDOM.createRoot(
