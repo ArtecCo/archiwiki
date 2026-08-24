780a9c2f5e2e75a23135ebc36f0fc525b62cdf3c
Artec.Co <artec.co.online@gmail.com>
2026-08-24 13:15:36 +0530

    Fix production build by removing unsupported jsPDF import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import "./pdfSafety.js";
 import "./sameNoteAnchors.js";
 import "./wikiEnhancements.js";
 import "./finalUiFixes.js";
