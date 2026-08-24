a903d2ecb7c61d6e89b2c2a3f7c7968ea3cc45e6
Artec.Co <artec.co.online@gmail.com>
2026-08-24 13:00:35 +0530

    Load PDF wrapping safety layer

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "./pdfSafety.js";
 import "./sameNoteAnchors.js";
 import "./wikiEnhancements.js";
 import "./finalUiFixes.js";
