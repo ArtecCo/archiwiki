a2ba4e44149c5aa8c43737de21ba08e2be085617
Artec.Co <artec.co.online@gmail.com>
2026-08-25 15:13:12 +0530

    top padding for editor reduced

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -2716,7 +2716,7 @@ return (
         }}
       >
         {isEditing ? (
-          <div className="flex-1 min-h-0 flex flex-col gap-4 relative">
+          <div className="flex-1 min-h-0 flex flex-col gap-1 relative">
 
             {/* TITLE */}
 
